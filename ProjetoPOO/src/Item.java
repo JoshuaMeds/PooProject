0ab081084b89fe2a234f329d9f1d4571c7bbe8e6
Joshua Mendes <161641155+JoshuaMeds@users.noreply.github.com>
2024-10-24 09:00:19 -0300

    classe estoque e Item

--- ProjetoPOO/src/Item.java
+++ ProjetoPOO/src/Item.java
@@ -1,2 +1,31 @@
 public class Item {
+    private String nomeItem;
+    private String descricao;
+    private String categoria;
+    public Item (String nomeItem, String descricao, String categoria){
+        this.nomeItem = nomeItem;
+        this.descricao = descricao;
+        this.categoria =categoria;
+
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public String getDecricao() {
+        return descricao;
+    }
+    public String getNomeItem() {
+        return nomeItem;
+    }
 }
