5a95021903298df633a56a5ed14db2cf74ea82d0
kempa9077 <157315735+kempa9077@users.noreply.github.com>
2024-10-24 10:31:44 -0300

    Implementacao classe Log e TxtLog; atualizacao diagrama de classes

--- ProjetoPOO/src/Log.java
+++ ProjetoPOO/src/Log.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Log {
+    private Funcionario f;
+    private Usuario u;
+    private Item i;
+    private Estoque e;
+    LocalDateTime d;
+
+    private String log;
+
+    public Log(){
+
+    }
+
+    public String logAdicionar(Funcionario f, Item i, Estoque e){
+        d = LocalDateTime.now();
+
+        return System.out.printf("%s - %s - %s - %s", f.nome, i.nome, e.bloco, d ).toString();
+    }
 }
