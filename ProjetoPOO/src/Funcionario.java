a29ca9aab2d2f4a86d659028d611e2fde773e722
kempa9077 <157315735+kempa9077@users.noreply.github.com>
2024-10-24 08:46:00 -0300

    Implementacao classe Funcionario e atualizacao diagrama de classes

--- ProjetoPOO/src/Funcionario.java
+++ ProjetoPOO/src/Funcionario.java
@@ -1,2 +1,40 @@
 public class Funcionario extends Pessoa {
+    protected int registro;
+
+    Funcionario(String cpf, String nome, int registro){
+        super(cpf, nome);
+        this.registro = registro;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public void registrarPerda(Pessoa p){
+
+    }
+
+    public void registrarRetirada(Pessoa p){
+
+    }
+
+    public void adicionarEstoque(Estoque e, Item i){
+
+    }
+
+    public void retirarEstoque(Item i){
+
+    }
+
+    public void visualizarRetiradas(){
+
+    }
+
+    public void alterarCredenciais(Pessoa p){
+
+    }
 }
