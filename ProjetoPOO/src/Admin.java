a3d2a65b48c8a96381095ab15bcdca8358c8cb9d
DaviMartins26 <davi.martins15@outlook.com>
2024-10-24 10:29:08 -0300

    Metodo AlterarCredenciais
    Com Polimorfia aplicada

--- ProjetoPOO/src/Admin.java
+++ ProjetoPOO/src/Admin.java
@@ -1,2 +1,11 @@
 public class Admin extends Funcionario {
+
+    public Admin(String cpf,String nome,int registro){
+        super(cpf,nome,registro);
+    }
+
+    public void alterarCredenciais(Pessoa p, String novoNome) {
+        p.setNome(novoNome);
+        System.out.println("Nome alterado para: " + novoNome);
+    }
 }
