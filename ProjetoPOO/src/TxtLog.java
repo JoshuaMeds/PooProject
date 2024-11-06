0a5bcb19e2352c4700944aff18d63aa5534041eb
Joshua Mendes <161641155+JoshuaMeds@users.noreply.github.com>
2024-11-06 10:20:40 -0300

    commit dnv que tava desmarcado ;(

--- ProjetoPOO/src/TxtLog.java
+++ ProjetoPOO/src/TxtLog.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class TxtLog {
     private ArrayList<Log> arrayListLog = new ArrayList<Log>();
@@ -15,6 +17,7 @@ public class TxtLog {
     public void apagarTxt(){
 
     }
+    // lê o que ta dentro de um arquivo txt
     public static String leitor(String caminhoArquivo) throws IOException {
         BufferedReader leitura = null;
         StringBuilder conteudo = new StringBuilder();
@@ -31,6 +34,7 @@ public class TxtLog {
         }
         return conteudo.toString();
     }
+    // escreve algo dentro de um arquivo txt
     public void escritor(String caminhoArquivo, String conteudo) throws IOException {
         BufferedWriter escritura = null;
         try {
@@ -39,6 +43,28 @@ public class TxtLog {
         } finally {
             if (escritura != null) {
                 escritura.close();
+            }
+        }
+    }
+    // lê e printa dentro da ide o conteudo de uma linha expecifica dentro do arquivo itens.txt
+    public class LeituraArquivo {
+
+        public static String buscarLinhaItens(String caminhoArquivo, String idProcurado) {
+            caminhoArquivo = "src/itens.txt";
+            try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {
+                String linha;
+
+                while ((linha = br.readLine()) != null) {
+                    // Verifica se a linha começa com "Nome: " e contém o nome procurado
+                    if (linha.startsWith("ID: " + idProcurado)) {
+                        return linha;
+                    }
+                }
+
+                return "Nome não encontrado no arquivo.";
+
+            } catch (IOException e) {
+                return "Erro ao ler o arquivo: " + e.getMessage();
             }
         }
     }
